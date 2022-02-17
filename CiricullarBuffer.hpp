bc61a8e345bd6ce1e266ce851ae6f94380d8d12b
ZexUlt <danil.chernozubov66@gmail.com>
2022-02-17 23:30:27 +0300

    [Fix] Fixed iterators [Fix] Fixed push_front() class-method [New] Reworked size() class-method, now actual size of the container calculates during addition or removing of elements

--- CiricullarBuffer.hpp
+++ CiricullarBuffer.hpp
@@ -12,6 +12,7 @@ private:
     T _data[SIZE]; // Buffer itself
     uint16_t _head; 
     uint16_t _tail;
+    uint16_t _size;
     bool isFull;
 
 public:
@@ -30,7 +31,7 @@ public:
         using pointer           = T*;
         using reference         = T&;
 
-        iterator(pointer ptr, uint16_t offset) : _ptr(ptr), _offset(offset){}
+        iterator(pointer init_ptr, pointer ptr, uint16_t offset) : _init_ptr(init_ptr), _ptr(ptr), _offset(offset){}
 
         self_type& operator=(self_type& other)
         {
@@ -45,8 +46,8 @@ public:
 
         self_type& operator++() // Prefix increment
         { 
-            this->_ptr++;
-            this->_offset = (this->_offset + 1) % SIZE; 
+            this->_offset++;
+            this->_ptr = this->_init_ptr + this->_offset;
             return *this; 
         } 
         self_type& operator++(int)  // Postfix increment
@@ -56,11 +57,18 @@ public:
             return tmp; 
         }
 
-        bool operator==(const self_type& other) { return (this->_ptr == other._ptr) && (this->_offset == other._offset); }
+        bool operator==(const self_type& other) 
+        {
+            // std::cout << this->_offset << " " << other._offset << '\n';
+            // std::cout << this->_ptr << " " << other._ptr << '\n';
+            // std::cout << _init_ptr << " " << other._init_ptr << '\n'; 
+            return (this->_init_ptr == other._init_ptr) && (this->_ptr == other._ptr) && (this->_offset == other._offset); 
+        }
         bool operator!=(const self_type& other) { return !(*this == other); }
 
     private:
         pointer _ptr;
+        const pointer _init_ptr;
         uint16_t _offset;
     };
     /** ITERATOR DEFINITION END **/
@@ -77,31 +85,34 @@ public:
         this->_head = (this->_head + 1) % SIZE;
 
         this->isFull = (this->_head == this->_tail);
+        this->_size += static_cast<int>(this->_size != SIZE); // Имеются сомнения насчет этого каста, может лучше переписать через if?
     }
 
     void push_front(T item)
     {
-        this->_tail = (SIZE + (this->_tail - 1) % SIZE);
-    
+        this->_tail = (SIZE + (this->_tail - 1) % SIZE) % SIZE;
+
         if(this->isFull){   
-            this->_head = (SIZE + (this->_head - 1) % SIZE);
+            this->_head = (SIZE + (this->_head - 1) % SIZE) % SIZE;
         }
 
         this->_data[this->_tail] = item;
 
         this->isFull = (this->_head == this->_tail);
+        this->_size += static_cast<int>(this->_size != SIZE); // Имеются сомнения насчет этого каста, может лучше переписать через if?
     }
 
     T pop_front()
     {
-       if(this->empty()){
-        return T();
+        if(this->empty()){
+            return T();
         }
         
         // Get data and pop element from buffer (move tail)
         auto val = this->_data[this->_tail];
         this->isFull = false;
         this->_tail = (this->_tail + 1) % SIZE;
+        this->_size--;
 
         return val; 
     }
@@ -110,6 +121,7 @@ public:
     {
         this->_head = this->_tail;
         this->isFull = false;
+        this->_size = 0;
     }
 
     // Element access
@@ -117,35 +129,35 @@ public:
     T back() const { return this->_data[this->_head]; }
     T operator[](int idx)
     {
-        assert(idx < SIZE);
+        assert(idx < this->_size);
         return this->_data[(this->_tail + idx) % SIZE];
     }
     const T operator[](int idx) const
     {
-        assert(idx < SIZE);
+        assert(idx < this->_size);
         return this->_data[(this->_tail + idx) % SIZE];
     } 
 
     // Iteration
-    auto begin() {return iterator(&(this->_data[this->_tail]), 0); }
-    auto end() { return iterator(&(this->_data[this->_head]), this->size()); }
+    auto begin() { return iterator(this->_data, &(this->_data[this->_tail]), 0); }
+    auto end() { return iterator(this->_data, &(this->_data[this->_tail + this->_size]), this->_size); }
 
     // State determination
     bool empty() const { return (!this->isFull && (this->_head == this->_tail)); }
     bool full() const { return this->isFull; }
     uint16_t size() const
     {
-        uint16_t __size = SIZE;
+        // uint16_t __size = SIZE;
 
-        if(!this->isFull){
-            if(this->_head >= this->_tail){
-                __size = this->_head - this->_tail; 
-            }else{
-                __size = SIZE + this->_head - this->_tail;
-            }
-        }
+        // if(!this->isFull){
+        //     if(this->_head >= this->_tail){
+        //         __size = this->_head - this->_tail; 
+        //     }else{
+        //         __size = SIZE + this->_head - this->_tail;
+        //     }
+        // }
 
-        return __size;
+        return _size;
     }
     constexpr uint16_t capacity() const { return SIZE; }
 };
--- main.cpp
+++ main.cpp
@@ -13,9 +13,9 @@ int main(int argc, char** argv)
     std::cout << "Size: " << buf.size() << '\n';
     
     buf.push_back(1); // 1
-    std::cout << buf[0] << " " << buf[1] << " " << buf[2] << '\n';
+    std::cout << buf[0] << '\n';
     buf.push_back(2); // 1 2
-    std::cout << buf[0] << " " << buf[1] << " " << buf[2] << '\n';
+    std::cout << buf[0] << " " << buf[1] << '\n';
     buf.push_back(3); // 1 2 3
     std::cout << buf[0] << " " << buf[1] << " " << buf[2] << '\n';
     buf.push_back(4); // 2 3 4
@@ -28,6 +28,8 @@ int main(int argc, char** argv)
     std::cout << buf[0] << " " << buf[1] << " " << buf[2] << '\n';
     buf.push_front(1); // 1 2 5
     std::cout << buf[0] << " " << buf[1] << " " << buf[2] << '\n';
+    buf.push_front(3); // 3 1 2
+    std::cout << buf[0] << " " << buf[1] << " " << buf[2] << '\n';
 
     std::cout << "Is empty: " << buf.empty() << '\n';
     std::cout << "Is full: " << buf.full() << '\n';
@@ -36,12 +38,49 @@ int main(int argc, char** argv)
     
     // std::cout << *buf.begin() << " " << *(++buf.begin()) << " " << *(buf.end() - 1);
     auto it = buf.begin();
-
+    /*
+        В общем, проблема такая: неправильно сравниваются итераторы, из-за чего получается, что они никогда не будут равны
+        Ниже в комментах написан вывод, который пишется из оператора сравнения итераторов
+        Получается так, что, когда оффсеты равны, адреса не равны
+        Потому что _head (конец очереди) стоит всегда на первом элементе при заполненном буфере
+        А когда _tail (начало очереди) доходит до _head, его оффсет сбрасывается в 0 (что верно вообще)
+    */
+    /**
+     * 0 2
+     * 0x7ffee35496c0 0x7ffee35496c0
+     * 0x7ffee35496c0 0x7ffee35496c0
+     * **/
+    std::cout << (it == buf.end()) << "\n\n";
     std::cout << *it << '\n';
-
     ++it;
+    /**
+     * 1 2
+     * 0x7ffee35496c4 0x7ffee35496c0
+     * 0x7ffee35496c0 0x7ffee35496c0
+     * **/
+    std::cout << (it == buf.end()) << "\n\n";
+    std::cout << *it << '\n';
+    ++it;
+    /**
+     * 2 2
+     * 0x7ffee35496c8 0x7ffee35496c0
+     * 0x7ffee35496c0 0x7ffee35496c0
+     *  **/
+    std::cout << (it == buf.end()) << "\n\n";
+    std::cout << *it << '\n';
+    ++it;
+    std::cout << *buf.end() << '\n';
+    /**
+     * 0 2
+     * 0x7ffee35496c0 0x7ffee35496c0
+     * 0x7ffee35496c0 0x7ffee35496c0
+     * **/
+    std::cout << (it == buf.end()) << "\n\n";
 
-    std::cout << *it;
+
+    for(auto it = buf.begin(); it != buf.end(); ++it){
+        std::cout << *it << " ";
+    }
 
     std::cout << '\n';
     return 0;
