682f95b3626edd933fcc785761b7e08997ac797a
ZexUlt <danil.chernozubov66@gmail.com>
2022-02-19 20:26:11 +0300

    [Fix] Fixed a typo in class name

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
-#include "CiricullarBuffer.hpp"
+#include "CircularBuffer.hpp"
 #define BOOST_TEST_MODULE Testing
 #include "boost/test/unit_test.hpp"
 
 BOOST_AUTO_TEST_CASE( constructors_test )
 {
-    CiricullarBuffer<3, int> rb;
+    CircularBuffer<3, int> rb;
     BOOST_CHECK_EQUAL(rb.empty(), true);
     BOOST_CHECK_EQUAL(rb.full(), false);
     BOOST_CHECK_EQUAL(rb.size(), 0);
 
-    CiricullarBuffer<3, int> rb1{1, 2, 3};
+    CircularBuffer<3, int> rb1{1, 2, 3};
     BOOST_CHECK_EQUAL(rb1.empty(), false);
     BOOST_CHECK_EQUAL(rb1.full(), true);
     BOOST_CHECK_EQUAL(rb1.size(), 3);
@@ -21,7 +21,7 @@ BOOST_AUTO_TEST_CASE( constructors_test )
     BOOST_CHECK_EQUAL(rb.full(), false);
     BOOST_CHECK_EQUAL(rb.size(), 0);
 
-    CiricullarBuffer<3, int> rb2(rb1);
+    CircularBuffer<3, int> rb2(rb1);
     BOOST_CHECK_EQUAL(rb2.empty(), false);
     BOOST_CHECK_EQUAL(rb2.full(), true);
     BOOST_CHECK_EQUAL(rb2.size(), 3);
@@ -38,7 +38,7 @@ BOOST_AUTO_TEST_CASE( constructors_test )
 
 BOOST_AUTO_TEST_CASE( iterators_test )
 {
-    CiricullarBuffer<3, int> rb{1, 2, 3};
+    CircularBuffer<3, int> rb{1, 2, 3};
     BOOST_REQUIRE_EQUAL(*rb.begin(), 1);
     auto it = rb.begin();
     BOOST_CHECK_EQUAL(*it, 1);
@@ -65,7 +65,7 @@ BOOST_AUTO_TEST_CASE( iterators_test )
 
 BOOST_AUTO_TEST_CASE( container_modification_test )
 {
-    CiricullarBuffer<3, int> rb;
+    CircularBuffer<3, int> rb;
 
     rb.push_back(1); // 1
     BOOST_REQUIRE_EQUAL(rb.back(), 1);
@@ -108,7 +108,7 @@ BOOST_AUTO_TEST_CASE( container_modification_test )
 
 BOOST_AUTO_TEST_CASE( element_access_test )
 {
-    CiricullarBuffer<3, int> rb{1, 2, 3};
+    CircularBuffer<3, int> rb{1, 2, 3};
 
     BOOST_REQUIRE_EQUAL(rb.front(), rb[0]);
     BOOST_REQUIRE_EQUAL(rb.back(), rb[2]);
